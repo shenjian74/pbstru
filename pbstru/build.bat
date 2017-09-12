b039b6325b8e4c7ba4ac574f9b4246e50554acd5
沈健10016632 <shen.jian1@zte.com.cn>
2017-09-12 14:00:54 +0800

    Fix compile problem in windows.

--- pbstru/build.bat
+++ pbstru/build.bat
@@ -1,7 +1,7 @@
-del /s /q /f build
-rd /s /q build
+del /S /Q /F build
+rd /S /Q build
 md build
 cd build
 cmake -G "CodeBlocks - MinGW Makefiles" ..
-mingw32-make
+mingw32-make -j
 move pbstru.exe ../bin
