91d55d388a2303276147f57f117c51ea10f1501a
沈健10016632 <shen.jian1@zte.com.cn>
2017-12-11 15:09:24 +0800

    Change two field to optional.

--- pbstru/bin/rm/codec/rmnotify.c
+++ pbstru/bin/rm/codec/rmnotify.c
@@ -3,15 +3,10 @@
 /* lint -save -e701 -e647 */
 
 void constru_message_RMNotify(st_rmnotify *var_RMNotify){
-    size_t i = 0;
-    for(i=0; i<PBSTRU_MAX_TIME_IN_RMNOTIFY; ++i){
-        constru_message_RmTime(&(var_RMNotify->var_time.item[i]));
-    }
-    var_RMNotify->var_time.count = 0;
-    for(i=0; i<PBSTRU_MAX_EVENT_IN_RMNOTIFY; ++i){
-        constru_message_RmEvent(&(var_RMNotify->var_event.item[i]));
-    }
-    var_RMNotify->var_event.count = 0;
+        constru_message_RmTime(&(var_RMNotify->var_time));
+    var_RMNotify->has_time = FALSE;
+        constru_message_RmEvent(&(var_RMNotify->var_event));
+    var_RMNotify->has_event = FALSE;
 }
 
 void destru_message_RMNotify(st_rmnotify* var_RMNotify){
@@ -19,22 +14,20 @@ void destru_message_RMNotify(st_rmnotify* var_RMNotify){
 }
 
 void clear_message_RMNotify(st_rmnotify* var_RMNotify){
-    size_t i = 0;
     var_RMNotify->var_primary.length = 0;
     var_RMNotify->var_primary.data = NULL;
-    for(i=0; i<var_RMNotify->var_time.count; ++i){
-        clear_message_RmTime(&(var_RMNotify->var_time.item[i]));
+    if(TRUE == var_RMNotify->has_time){
+        clear_message_RmTime(&(var_RMNotify->var_time));
     }
-    var_RMNotify->var_time.count = 0;
-    for(i=0; i<var_RMNotify->var_event.count; ++i){
-        clear_message_RmEvent(&(var_RMNotify->var_event.item[i]));
+    var_RMNotify->has_time = FALSE;
+    if(TRUE == var_RMNotify->has_event){
+        clear_message_RmEvent(&(var_RMNotify->var_event));
     }
-    var_RMNotify->var_event.count = 0;
+    var_RMNotify->has_event = FALSE;
 }
 
 size_t encode_message_RMNotify(const st_rmnotify* var_RMNotify, BYTE* buf){
     size_t encode_buf_len;
-    size_t i;
     size_t offset = 0;
 
     /* tag:1 type:bytes */
@@ -45,24 +38,24 @@ size_t encode_message_RMNotify(const st_rmnotify* var_RMNotify, BYTE* buf){
     }
     offset += var_RMNotify->var_primary.length;
 
-    for(i = 0; i < var_RMNotify->var_time.count; ++i){
+    if(var_RMNotify->has_time){
         /* tag:2 type:message */
         encode_tag_byte(buf, 2, WIRE_TYPE_LENGTH_DELIMITED, &offset);
-        encode_buf_len = encode_message_RmTime(&(var_RMNotify->var_time.item[i]), NULL);
+        encode_buf_len = encode_message_RmTime(&(var_RMNotify->var_time), NULL);
         encode_varint(encode_buf_len, buf, &offset);
         if(NULL != buf){
-            encode_message_RmTime(&(var_RMNotify->var_time.item[i]), buf + offset);
+            encode_message_RmTime(&(var_RMNotify->var_time), buf + offset);
         }
         offset += encode_buf_len;
     }
 
-    for(i = 0; i < var_RMNotify->var_event.count; ++i){
+    if(var_RMNotify->has_event){
         /* tag:3 type:message */
         encode_tag_byte(buf, 3, WIRE_TYPE_LENGTH_DELIMITED, &offset);
-        encode_buf_len = encode_message_RmEvent(&(var_RMNotify->var_event.item[i]), NULL);
+        encode_buf_len = encode_message_RmEvent(&(var_RMNotify->var_event), NULL);
         encode_varint(encode_buf_len, buf, &offset);
         if(NULL != buf){
-            encode_message_RmEvent(&(var_RMNotify->var_event.item[i]), buf + offset);
+            encode_message_RmEvent(&(var_RMNotify->var_event), buf + offset);
         }
         offset += encode_buf_len;
     }
@@ -88,23 +81,17 @@ BOOL decode_message_RMNotify(BYTE* buf, const size_t buf_len, st_rmnotify* var_R
             break;
         /* type:message */
         case 2:
-            if(var_RMNotify->var_time.count >= PBSTRU_MAX_TIME_IN_RMNOTIFY) {
-                return FALSE;  /* 数组超限 */
-            }
             decode_varint(buf + offset, &tmp_field_len, &offset);
-            decode_message_RmTime(buf + offset, tmp_field_len, &(var_RMNotify->var_time.item[var_RMNotify->var_time.count]));
+            decode_message_RmTime(buf + offset, tmp_field_len, &(var_RMNotify->var_time));
             offset += tmp_field_len;
-            var_RMNotify->var_time.count += 1;
+            var_RMNotify->has_time = TRUE;
             break;
         /* type:message */
         case 3:
-            if(var_RMNotify->var_event.count >= PBSTRU_MAX_EVENT_IN_RMNOTIFY) {
-                return FALSE;  /* 数组超限 */
-            }
             decode_varint(buf + offset, &tmp_field_len, &offset);
-            decode_message_RmEvent(buf + offset, tmp_field_len, &(var_RMNotify->var_event.item[var_RMNotify->var_event.count]));
+            decode_message_RmEvent(buf + offset, tmp_field_len, &(var_RMNotify->var_event));
             offset += tmp_field_len;
-            var_RMNotify->var_event.count += 1;
+            var_RMNotify->has_event = TRUE;
             break;
         default:
         	deal_unknown_field(wire_type, buf+offset, &offset);
