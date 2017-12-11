3a010d1be6f895e34a4bad3aacbe7ae56ccbc2b4
沈健10016632 <shen.jian1@zte.com.cn>
2017-12-11 15:09:24 +0800

    Change two field to optional.

--- pbstru/bin/rm/codec/rmnotify.h
+++ pbstru/bin/rm/codec/rmnotify.h
@@ -12,27 +12,17 @@ extern "C"{
 /*
 message RMNotify {
   required bytes primary = 1;
-  repeated .rm.RmTime time = 2;
-  repeated .rm.RmEvent event = 3;
+  optional .rm.RmTime time = 2;
+  optional .rm.RmEvent event = 3;
 }
 */
-#define PBSTRU_MAX_TIME_IN_RMNOTIFY 10
-#define PBSTRU_MAX_EVENT_IN_RMNOTIFY 10
-
-typedef struct _st_time_in_rmnotify_rmtime_list {
-    size_t count;
-    st_rmtime item[PBSTRU_MAX_TIME_IN_RMNOTIFY];  /* tag:2 type:message */
-} st_time_in_rmnotify_rmtime_list;
-
-typedef struct _st_event_in_rmnotify_rmevent_list {
-    size_t count;
-    st_rmevent item[PBSTRU_MAX_EVENT_IN_RMNOTIFY];  /* tag:3 type:message */
-} st_event_in_rmnotify_rmevent_list;
 
 typedef struct _st_rmnotify {
     ps_bytes var_primary;  /* tag:1 */
-    st_time_in_rmnotify_rmtime_list var_time;  /* tag:2 */
-    st_event_in_rmnotify_rmevent_list var_event;  /* tag:3 */
+    BOOL has_time;
+    st_rmtime var_time;  /* tag:2 */
+    BOOL has_event;
+    st_rmevent var_event;  /* tag:3 */
 } st_rmnotify;
 
 /* construct msg when first use, call clear_message_*() internaly. */
