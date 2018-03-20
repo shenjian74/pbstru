7c238ed71e5f031d3b98f8386d4c9a8d4bebcb95
沈健10016632 <shen.jian1@zte.com.cn>
2018-03-20 16:57:52 +0800

    Use RAII to prevent memory leak.

--- pbstru/pbstru/pbstru.cpp
+++ pbstru/pbstru/pbstru.cpp
@@ -1730,9 +1730,18 @@ private:
     }
 };
 
+/* RAII */
+static inline void freep(char **p) { 
+    if(*p){  
+        free(*p);  
+    }
+    *p = NULL; 
+}
+#define _cleanup_free_ __attribute__((cleanup(freep)))
+
 int create_path(CBString &path)
 {  
-    char dir_name[1024];
+    _cleanup_free_ char* dir_name = (char *)malloc(path.length() + 10);
     if(path[path.length()-1] != path_sep)  
     {
         path += path_sep;
