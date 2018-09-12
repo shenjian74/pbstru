d52f4ed2a16b81d85db133aa2a8f159270d0f8ff
沈健10016632 <shen.jian1@zte.com.cn>
2018-09-12 09:03:32 +0800

    Add git version to repository.

--- pbstru/build.sh
+++ pbstru/build.sh
@@ -1,3 +1,9 @@
+#!/bin/sh
+
+# Add current build time
+current_time=`date +"%Y-%m-%d %H:%M:%S"`
+sed s/MYVERSION/"build: $current_time"/g pbstru/version.h.tmp > pbstru/version.h
+
 # Install static library at first.
 # -- sudo yum install libstdc++-static glibc-static
 rm -rf build
@@ -6,3 +12,4 @@ cd build
 cmake ..
 make
 mv pbstru ../bin
+#!/bin/sh
