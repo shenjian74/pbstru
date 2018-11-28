f7c38103b8df3ee138ee546df3ca9db9d4e1f178
沈健10016632 <shen.jian1@zte.com.cn>
2018-11-28 16:12:27 +0800

    Modified   build.sh Modified   test/CMakeLists.txt
    Signed-off-by: 沈健10016632 <shen.jian1@zte.com.cn>

--- build.sh
+++ build.sh
@@ -8,9 +8,19 @@
 # -- sudo yum install libstdc++-static glibc-static
 
 if [ ! -f "protobuf3/Makefile" ]; then 
-  wget --timestamping https://github.com/protocolbuffers/protobuf/releases/download/v3.5.1/protobuf-all-3.5.1.tar.gz && tar xvfz protobuf-all-3.5.1.tar.gz && mv protobuf-3.5.1 protobuf3 && cd protobuf3 && ./autogen.sh && ./configure && make
+  wget --timestamping https://github.com/protocolbuffers/protobuf/releases/download/v3.5.1/protobuf-all-3.5.1.tar.gz && \
+  tar xvfz protobuf-all-3.5.1.tar.gz && \
+  mv protobuf-3.5.1 protobuf3 && \
+  cd protobuf3 && \
+  ./autogen.sh && \
+  ./configure
 fi
 
 if [ -f "protobuf3/Makefile" ]; then 
-  rm -rf build && mkdir build && cd build && cmake .. && make && mv pbstru ../bin
+  rm -rf build && \
+  mkdir build && \
+  cd build && \
+  cmake .. && \
+  make && \
+  mv pbstru ../bin
 fi
