0dbcd13ec36f7f65300afa118ebef2e197740603
沈健10016632 <shen.jian1@zte.com.cn>
2020-11-26 14:17:25 +0800

    增加cudr_emc.proto定义
    Signed-off-by: 沈健10016632 <shen.jian1@zte.com.cn>

--- bin/gen_codec.bat
+++ bin/gen_codec.bat
@@ -1,4 +1,4 @@
-pbstru.exe NF cdb.proto cdb_ccc.proto Http2Proxy.Http2AppReq.proto Http2Proxy.Http2Common.proto codec
+pbstru.exe NF cdb.proto cdb_ccc.proto Http2Proxy.Http2AppReq.proto Http2Proxy.Http2Common.proto cudr_emc.proto codec
 pbstru.exe NF lb.proto lb
 pbstru.exe NF cdb.proto cdap
 echo call pbstru done.
--- bin/gen_codec.sh
+++ bin/gen_codec.sh
@@ -1,4 +1,4 @@
-./pbstru NF cdb.proto cdb_ccc.proto Http2Proxy.Http2AppReq.proto Http2Proxy.Http2Common.proto ./codec
+./pbstru NF cdb.proto cdb_ccc.proto Http2Proxy.Http2AppReq.proto Http2Proxy.Http2Common.proto cudr_emc.proto ./codec
 ./pbstru NF lb.proto ./lb
 ./pbstru NF cdb.proto ./cdap
 echo call pbstru done.
