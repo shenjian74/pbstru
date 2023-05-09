84b5e362f934c1b41ecd6d2278ef619a7a0b3e55
沈健10016632 <shen.jian1@zte.com.cn>
2023-05-09 10:51:00 +0800

    Modified   bin/gen_codec.sh

--- bin/gen_codec.sh
+++ bin/gen_codec.sh
@@ -1,4 +1,5 @@
 ./pbstru NF cdb.proto cdb_ccc.proto Http2Proxy.Http2AppReq.proto Http2Proxy.Http2Common.proto cudr_emc.proto ./codec
 ./pbstru NF lb.proto ./lb
 ./pbstru NF cdb.proto ./cdap
+./pbstru NF snf.proto ./codec
 echo call pbstru done.
